0e32955b4e95b12b67eca0e0a9d929cc4e11d773
Aditya Balasubramanian <adityabala2005@gmail.com>
2022-12-31 14:13:06 +0400

    added new host to ALLOWED_HOSTS

--- bank_management/settings.py
+++ bank_management/settings.py
@@ -35,11 +35,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
+    'bankofbrs-6oblu.ondigitalocean.app',
     'brsbank.herokuapp.com'
   ]
 
